bff5793640edfa45cef98ddce18d9c19db5a1f0f
Nithesh Shettigar <nihsh.k@gmail.com>
2019-09-18 09:51:58 +0530

    Message model commit

---
+++ src/main/java/org/smartworld/spring/kafka/model/MessageModel.java
@@ -0,0 +1,12 @@
+package org.smartworld.spring.kafka.model;
+
+public class MessageModel {
+
+    private String id;
+    private String name;
+
+    public MessageModel(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
