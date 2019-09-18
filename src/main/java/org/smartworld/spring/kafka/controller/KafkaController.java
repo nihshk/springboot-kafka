ec49e6406bdb9378061205dff992eb960c1bced1
Nithesh Shettigar <nihsh.k@gmail.com>
2019-09-18 09:27:43 +0530

    Initial commit

---
+++ src/main/java/org/smartworld/spring/kafka/controller/KafkaController.java
@@ -0,0 +1,11 @@
+package org.smartworld.spring.kafka.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/kafka")
+public class KafkaController {
+
+}
