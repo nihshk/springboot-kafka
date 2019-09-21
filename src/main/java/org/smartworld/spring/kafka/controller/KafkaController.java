943bfbf1d3b48ce3f9eb664c780e972ae9e9b343
Nithesh Shettigar <nihsh.k@gmail.com>
2019-09-21 11:32:27 +0530

    First Execution of application

--- src/main/java/org/smartworld/spring/kafka/controller/KafkaController.java
+++ src/main/java/org/smartworld/spring/kafka/controller/KafkaController.java
@@ -4,6 +4,7 @@ package org.smartworld.spring.kafka.controller;
 import org.smartworld.spring.kafka.model.MessageModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class KafkaController {
     @Autowired
     private KafkaTemplate<String, MessageModel> kafkaTemplate;
 
+    @PostMapping
     public void postMessage(@RequestBody MessageModel messageModel){
 
         kafkaTemplate.send("msgTopic", messageModel);
